5352449cb501addae92ae9c79222a84c0040032c
siios <siios.contact@gmail.com>
2022-10-21 11:42:08 +0200

    init

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -6,26 +6,22 @@ import {
   ProductTitle,
 } from "../components";
 import { products } from "../data/products";
-import { useShoppingCart } from "../hooks/useShoppingCart";
 import "../styles/custom-styles.css";
 
+const product = products[0];
 
 export const ShoppingPage = () => {
   
-  const {onProductCountChange, shoppingCart} = useShoppingCart();
 
   return (
     <div>
       <h1>Shopping Store</h1>
       <hr />
-      <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
-        {products.map((product) => (
+
           <ProductCard
-            onChange={onProductCountChange}
             product={product}
             className="text-white bg-dark"
             key={product.id}
-            value={shoppingCart[product.id]?.count || 0}
           >
             <ProductImage
               className="custom-img"
@@ -34,29 +30,7 @@ export const ShoppingPage = () => {
             <ProductTitle className="text-white text-bold" />
             <ProductButtons className="custom-buttons" />
           </ProductCard>
-        ))}
-      </div>
-      <div className="shopping-cart">
-        {Object.entries(shoppingCart).map(([key, product]) => (
-          <ProductCard
-            onChange={onProductCountChange}
-            product={product}
-            className="text-white bg-dark"
-            key={key}
-            style={{ width: "100px" }}
-            value={product.count}
-          >
-            <ProductImage
-              className="custom-img"
-              style={{ boxShadow: "10px 10px 10px rgba(0,0,0,0.2)" }}
-            />
-            <ProductButtons
-              className="custom-buttons"
-              style={{ display: "flex", justifyContent: "center" }}
-            />
-          </ProductCard>
-        ))}
-      </div>
+
     </div>
   );
 };
